8de59373b3e9df097718c2317d5bbb154763db59
radha <radhabarsaiya@gmail.com>
2024-07-24 18:30:00 +0530

    final_plot_image

--- Images_ploting/plotting.py
+++ Images_ploting/plotting.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 from datetime import datetime
 
 def plot(img_path, record_id, speed, speed_zone, distance, direction, veh_type, veh_no, veh_ctg, voilation, operator_name, op_id, lat, long, col_b, col_g, col_r,loc):
@@ -9,13 +10,19 @@ def plot(img_path, record_id, speed, speed_zone, distance, direction, veh_type, 
     font = cv2.FONT_HERSHEY_SIMPLEX 
     date_time= datetime.now()
     date_time_format = date_time.strftime("%d-%m-%Y %H:%M")  
-    borderd_img = cv2.copyMakeBorder(img_read,30,60,0,0,borderType=cv2.BORDER_CONSTANT)
-    cv2.putText(borderd_img, date_time_format, (1000,25),font, fontScale, color, thickness)
-    cv2.putText(borderd_img, f"RECORD ID: {record_id}", (10,25),font, fontScale, color, thickness)
-    cv2.putText(borderd_img, f"SPEED:{speed} (SPEED ZONE: {speed_zone})", (10,55),font, fontScale, color, thickness)
-    cv2.putText(borderd_img, f"DISTANCE: {distance} | DIRECTION: {direction}", (700,55),font, fontScale, color, thickness)
-    cv2.putText(borderd_img, f"LAT: {lat} | LONG: {long}", (420,25),font, fontScale, color, thickness)
-    cv2.putText(borderd_img, f"VEHICLE TYPE: {veh_type} ({veh_ctg}) , VEHICLE NO: {veh_no}", (10,740),font, fontScale, color, thickness)
-    cv2.putText(borderd_img, f"VIOLATIONS: {voilation} ", (10,770),font, fontScale, color, thickness)
-    cv2.putText(borderd_img, f"OPERATOR NAME: {operator_name}, OPERATOR DEPARTMENT: {op_id}, LOCATION: {loc}", (10,800),font, fontScale, color, thickness)
-    cv2.imwrite("done_cust.png",borderd_img)
+    borderd_img = cv2.copyMakeBorder(img_read,30,30,0,0,borderType=cv2.BORDER_CONSTANT)
+    cv2.putText(borderd_img, date_time_format, (1000,55),font, fontScale, color, thickness)
+    cv2.putText(borderd_img, f"RECORD ID: {record_id}", (10,55),font, fontScale, color, thickness)
+    cv2.putText(borderd_img, f"SPEED:{speed} (SPEED ZONE: {speed_zone})", (10,85),font, fontScale, color, thickness)
+    cv2.putText(borderd_img, f"DISTANCE: {distance} | DIRECTION: {direction}", (700,85),font, fontScale, color, thickness)
+    cv2.putText(borderd_img, f"LAT: {lat} | LONG: {long}", (420,55),font, fontScale, color, thickness)
+    cv2.putText(borderd_img, f"VEHICLE TYPE: {veh_type} ({veh_ctg}) , VEHICLE NO: {veh_no}", (10,680),font, fontScale, color, thickness)
+    cv2.putText(borderd_img, f"VIOLATIONS: {voilation} ", (10,710),font, fontScale, color, thickness)
+    cv2.putText(borderd_img, f"OPERATOR NAME: {operator_name}, OPERATOR DEPARTMENT: {op_id}, LOCATION: {loc}", (10,740),font, fontScale, color, thickness)
+    overlay = np.zeros_like(borderd_img)
+    cv2.rectangle(overlay,(0,30),(1280,100), (255,255,255), -1)
+    cv2.rectangle(overlay,(0,650),(1280,750), (255,255,255), -1)
+    alpha = 0.9
+    final = cv2.addWeighted(borderd_img,alpha,overlay,1-alpha,0 )
+    # cv2.imwrite("plot_image.png",borderd_img)
+    cv2.imwrite("plot_image_output.png",final)
--- Images_ploting/plotting_logic.py
+++ Images_ploting/plotting_logic.py
@@ -1,45 +1,79 @@
+# import cv2
+# from datetime import datetime
+# img = r"C:\Users\hp\Downloads\Vinayan_New\Images_ploting\original_image.png"
+# # rocord_id =167803
+# # speed = '87km/h'
+# # speed_zone = '70km/h'
+# # distance = '457.89 M'
+# # direction = "jlkj;j;"
+# # veh_type = "CAR"
+# # veh_no = "DL9087"
+# # veh_ctg = "Private"
+# # voilation = "Overspeeding"
+# # operator_name ="Vinayan"
+# # op_id = 6789
+# # lat = 23.675699
+# # long = 77.8795645
+# img_read = cv2.imread(img)
+# # overlay = img_read.copy()
+# fontScale = 1
+# color = (255, 0, 0)
+# voilation_color = (0,0,255)                                                                                         
+# thickness = 2
+# # font = cv2.FONT_HERSHEY_SIMPLEX 
+# font = cv2.FONT_HERSHEY_PLAIN
+# # date_time= datetime.now()
+# # date_time_format = date_time.strftime("%d-%m-%Y %H:%M")
+# # # print(date_time_format)
+
+# # cv2.putText(img_read, date_time_format, (950,40),font, fontScale, color, thickness)
+# # cv2.putText(img_read, f"RECORD ID: {rocord_id}", (20,40),font, fontScale, color, thickness)
+# # cv2.putText(img_read, f"SPEED:{speed}, SPEED ZONE: {speed_zone}", (20,70),font, fontScale, voilation_color, thickness)
+# # cv2.putText(img_read, f"DISTANCE: {distance}, DIRECTION: {direction}", (20,100),font, fontScale, color, thickness)
+# # cv2.putText(img_read, f"LAT.: {lat}, LONG.: {long}", (20,130),font, fontScale, color, thickness)
+# # cv2.putText(img_read, f"VEHICLE TYPE: {veh_type} , VEHICLE NO.: {veh_no}", (20,650),font, fontScale, color, thickness)
+# # cv2.putText(img_read, f"VEHICLE CATEGORY: {veh_ctg}, VIOLATIONS: {voilation} ", (20,680),font, fontScale, voilation_color, thickness)
+# # cv2.putText(img_read, f"OPERATOR NAME: {operator_name}, OPERATOR ID: {op_id}", (20,710),font, fontScale, color, thickness)
+# # # cv2.imshow("win",img_read)
+# # # cv2.waitKey(0)
+# # # cv2.destroyAllWindows()
+# # cv2.imwrite("customize_img.png",img_read)
+# # # 
+# # # sp = img_read.shape
+# # # print(sp)
+# cns= cv2.copyMakeBorder(img_read,30,30,0,0,cv2.BORDER_CONSTANT)
+# cv2.putText(cns, f"DISTANCE: direction", (20,25),font, fontScale, color, thickness)
+# cv2.putText(cns, f"VEHICLE TYPE:  , VEHICLE NO:", (10,740),font, fontScale, color, thickness)
+# cv2.putText(cns, f"VIOLATIONS"  , (10,770),font, fontScale, color, thickness)
+# cv2.putText(cns, f"OPERATOR NAME: , OPERATOR DEPARTMENT: LOCATION:", (10,800),font, fontScale, color, thickness)
+# cv2.rectangle(cns,(0,30),(1050,60), (0,0,255),-1) 
+# alpha = 0.4
+# bg_img = cv2.addWeighted(cns, alpha, cns,1- alpha,0, 0)  
+# cv2.imwrite("new_ip.png",bg_img)
+
+
+
+
+
 import cv2
+import numpy as np
 from datetime import datetime
 img = r"C:\Users\hp\Downloads\Vinayan_New\Images_ploting\original_image.png"
-# rocord_id =167803
-# speed = '87km/h'
-# speed_zone = '70km/h'
-# distance = '457.89 M'
-# direction = "jlkj;j;"
-# veh_type = "CAR"
-# veh_no = "DL9087"
-# veh_ctg = "Private"
-# voilation = "Overspeeding"
-# operator_name ="Vinayan"
-# op_id = 6789
-# lat = 23.675699
-# long = 77.8795645
+
 img_read = cv2.imread(img)
+# overlay = np.zeros_like(img_read)
 fontScale = 1
 color = (255, 0, 0)
 voilation_color = (0,0,255)                                                                                         
 thickness = 2
-font = cv2.FONT_HERSHEY_SIMPLEX 
-# date_time= datetime.now()
-# date_time_format = date_time.strftime("%d-%m-%Y %H:%M")
-# # print(date_time_format)
-
-# cv2.putText(img_read, date_time_format, (950,40),font, fontScale, color, thickness)
-# cv2.putText(img_read, f"RECORD ID: {rocord_id}", (20,40),font, fontScale, color, thickness)
-# cv2.putText(img_read, f"SPEED:{speed}, SPEED ZONE: {speed_zone}", (20,70),font, fontScale, voilation_color, thickness)
-# cv2.putText(img_read, f"DISTANCE: {distance}, DIRECTION: {direction}", (20,100),font, fontScale, color, thickness)
-# cv2.putText(img_read, f"LAT.: {lat}, LONG.: {long}", (20,130),font, fontScale, color, thickness)
-# cv2.putText(img_read, f"VEHICLE TYPE: {veh_type} , VEHICLE NO.: {veh_no}", (20,650),font, fontScale, color, thickness)
-# cv2.putText(img_read, f"VEHICLE CATEGORY: {veh_ctg}, VIOLATIONS: {voilation} ", (20,680),font, fontScale, voilation_color, thickness)
-# cv2.putText(img_read, f"OPERATOR NAME: {operator_name}, OPERATOR ID: {op_id}", (20,710),font, fontScale, color, thickness)
-# # cv2.imshow("win",img_read)
-# # cv2.waitKey(0)
-# # cv2.destroyAllWindows()
-# cv2.imwrite("customize_img.png",img_read)
-# # 
-# # sp = img_read.shape
-# # print(sp)
+font = cv2.FONT_HERSHEY_SIMPLEX
 cns= cv2.copyMakeBorder(img_read,30,30,0,0,cv2.BORDER_CONSTANT)
 cv2.putText(cns, f"DISTANCE: direction", (20,25),font, fontScale, color, thickness)
-
-cv2.imwrite("new.png",cns)
+cv2.putText(cns, f"VEHICLE TYPE:  , VEHICLE NO:", (10,740),font, fontScale, color, thickness)
+cv2.putText(cns, f"VIOLATIONS"  , (10,770),font, fontScale, color, thickness)
+cv2.putText(cns, f"OPERATOR NAME: , OPERATOR DEPARTMENT: LOCATION:", (10,800),font, fontScale, color, thickness)
+overlay = np.zeros_like(cns)
+cv2.rectangle(overlay,(0,700),(1050,730), (255,255,255),-1) 
+alpha = 0.8
+bg_img = cv2.addWeighted(cns, alpha, overlay, 1 - alpha, 0)
+cv2.imwrite("overlay.png",bg_img)
